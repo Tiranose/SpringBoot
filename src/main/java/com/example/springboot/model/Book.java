5a6f63a495cb9edda7ad984d7ee11db5a8d710fe
homeworkfirst <zookotelif@gmail.com>
2025-02-16 18:23:22 +0200

    Implemented DTOs, MapStruct mapping, and BookController with CRUD endpoints

--- src/main/java/com/example/springboot/model/Book.java
+++ src/main/java/com/example/springboot/model/Book.java
@@ -1,10 +1,10 @@
 package com.example.springboot.model;
 
-import java.math.BigDecimal;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/com/example/springboot/service/BookService.java
+++ src/main/java/com/example/springboot/service/BookService.java
@@ -2,7 +2,6 @@ package com.example.springboot.service;
 
 import com.example.springboot.dto.BookDto;
 import com.example.springboot.dto.CreateBookRequestDto;
-
 import java.util.List;
 
 public interface BookService {
